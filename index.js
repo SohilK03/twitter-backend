23327e528ed4360ea281278c8aa4f4b1cded37c1
Sohil Khattar <sohilkhattar123@gmail.com>
2022-07-30 16:09:02 +0530

    User auth & OAuth2 added

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-// const express = require("express");
-import express from "express";
-import { PORT } from "./config/config.js";
-
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const { PORT, mongo_uri } = require("./config/config");
+const userRoutes = require("./Routes/userRoutes.js");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+mongoose
+  .connect(mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected To DB"))
+  .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport Config
+
+require("./config/passport.js")(passport);
 app.get("/", (req, res) => {
   res.send("Hello, Twitter Backend Running");
 });
+
+app.use("/users", userRoutes);
 app.listen(process.env.PORT || PORT, (req, res) => {
-  console.log(`Server Running on ${PORT}`);
+  console.log(`Server Running on ${process.env.PORT || PORT}`);
 });
