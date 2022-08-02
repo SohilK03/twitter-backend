5c5e7ad73595e5d885275318415cca957317c8a2
Sohil Khattar <sohilkhattar123@gmail.com>
2022-08-02 16:15:38 +0530

    fix fix

--- config/config.js
+++ config/config.js
@@ -1,6 +1,3 @@
-if (process.env.NODE_ENV === "production") keys = require("./config_prod");
-else
-  keys = require("./config_dev")
-    ? require("./config_dev")
-    : require("./config_prod");
-module.exports = keys;
+if (process.env.NODE_ENV === "production")
+  module.exports = require("./config_prod");
+else module.exports = require("./config_dev");
--- config/config_prod.js
+++ config/config_prod.js
@@ -1,1 +1,7 @@
-module.exports = process.env;
+module.exports = {
+  PORT: process.env.PORT,
+  mongo_uri: process.env.mongo_uri,
+  secretOrKey: process.env.secretOrKey,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+};
