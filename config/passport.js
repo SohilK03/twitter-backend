d6de0591bf3cac0d338369c6b14883c0395fcb3c
Sohil Khattar <sohilkhattar123@gmail.com>
2022-08-02 16:36:02 +0530

    fixed passport

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ const {
   secretOrKey,
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
-} = require("./config/config.js");
+} = require("./config.js");
 var GoogleStrategy = require("passport-google-oauth2").Strategy;
 var JwtStrategy = require("passport-jwt").Strategy,
   ExtractJwt = require("passport-jwt").ExtractJwt;
