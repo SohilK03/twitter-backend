45a2fbd898a74a5d6f9566408bf0283c505a86ba
Sohil Khattar <sohilkhattar123@gmail.com>
2022-08-02 16:12:12 +0530

    last try

--- config/config.js
+++ config/config.js
@@ -1,3 +1,6 @@
 if (process.env.NODE_ENV === "production") keys = require("./config_prod");
-else keys = require("./config_dev");
+else
+  keys = require("./config_dev")
+    ? require("./config_dev")
+    : require("./config_prod");
 module.exports = keys;
